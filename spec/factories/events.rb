5e658ecf819b5e1a9e7018137e24d886e07eaa2b
SarahPAms <Sarah.L.Polan@gmail.com>
2017-03-15 12:09:55 +0100

    Install FactoryGirl, Faker and define factories

---
+++ spec/factories/events.rb
@@ -0,0 +1,22 @@
+FactoryGirl.define do
+  factory :room do
+    name              { Faker::Educator.course }
+    description       { Faker::Lorem.sentence }
+    location          { Faker::Address.street_address }
+    price             { Faker::Commerce.price }
+    capacity          100
+    includes_food     false
+    includes_drink    true
+    starts_at         30.days.from_now
+    ends_at           30.days.from_now + 1.hour
+    user              { build(:user) }
+
+    trait :active do
+      active true
+    end
+
+    trait :inactive do
+      active false
+    end
+  end
+end
