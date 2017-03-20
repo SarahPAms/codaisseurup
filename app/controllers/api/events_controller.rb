7a5c3da231bcc9b75da8bd219eef7aa2bb97a522
SarahPAms <Sarah.L.Polan@gmail.com>
2017-03-20 16:55:18 +0100

    Add API events destroy and update

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,4 +1,6 @@
 class Api::EventsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def index
     render status: 200, json: {
       events: Event.all
@@ -27,7 +29,31 @@ class Api::EventsController < ApplicationController
     end
   end
 
-  private event_params
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+
+    render status: 200, json: {
+      message: "Event deleted"
+    }.to_json
+  end
+
+  def update
+    event = Event.find(params[:id])
+    if event.update(event_params)
+      render status: 201, json: {
+        message: "Event updated",
+        event: event
+      }.to_json
+    else
+      render status: 422, json:{
+        message: "The event could not be updated",
+        errors: event.errors
+      }.to_json
+    end
+  end
+
+  private
     def event_params
       params.require(:event).permit(:name, :description, :location, :price, :capacity, :includes_food, :includes_drinks, :starts_at, :ends_at, :active, category_ids: [])
     end
